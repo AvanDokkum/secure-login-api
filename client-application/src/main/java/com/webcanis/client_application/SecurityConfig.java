d7b5cbaa88aa66bb9c37dc3167820dec575b77ef
avandokkum <arjanvandokkum@gmail.com>
2024-11-27 10:34:18 +0100

    test : re-roll, added some authentication in client app, which is wrong I think.

--- client-application/src/main/java/com/webcanis/client_application/SecurityConfig.java
+++ client-application/src/main/java/com/webcanis/client_application/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.webcanis.client_application;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,10 +19,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/lessons").permitAll();
+                    authorize.anyRequest().authenticated();
+                        }
                 )
-                .csrf(csrf -> csrf.disable());
+                .oauth2Login(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
         return http.build();
     }
 }
